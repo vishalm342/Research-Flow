bc96e8e26072aeefcc5ae8b21c60d3dc207c79cc
vishalm342 <vishalm8656@gmail.com>
2026-05-05 23:20:51 +0530

    refactor: overhaul frontend theme to dark mode and update project status

--- researchflow/frontend/src/App.jsx
+++ researchflow/frontend/src/App.jsx
@@ -1,17 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import WorkspaceLayout from './layouts/WorkspaceLayout';
 import ChatWorkspace from './pages/ChatWorkspace';
+import LandingPage from './pages/LandingPage';
+import AuthPage from './pages/AuthPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Router>
-      <WorkspaceLayout>
-        <Routes>
-          <Route path="/" element={<ChatWorkspace />} />
-          <Route path="/c/:conversationId" element={<ChatWorkspace />} />
-        </Routes>
-      </WorkspaceLayout>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/app" element={
+          <ProtectedRoute>
+            <WorkspaceLayout>
+              <ChatWorkspace />
+            </WorkspaceLayout>
+          </ProtectedRoute>
+        } />
+        <Route path="/c/:conversationId" element={
+          <ProtectedRoute>
+            <WorkspaceLayout>
+              <ChatWorkspace />
+            </WorkspaceLayout>
+          </ProtectedRoute>
+        } />
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
